6435a81764181b6c087026485cff6035e4339e77
Ashwin Saval <ashwinsaval@gmail.com>
2015-01-16 22:20:09 +0530

    * BUGFIX -- Fixed bug where the Keypresses were disabled when the game wasn't running

--- javascripts/tetris_test_functions.js
+++ javascripts/tetris_test_functions.js
@@ -14,60 +14,48 @@ function printGrid(){
   }
 }
 var condition = 0;
+
 document.onkeydown = function(e) {
-    e = e || window.event;
+  e = e || window.event;
 
-    if(!runGame){
-      return false;
+  if(runGame){
+   switch(e.which || e.keyCode) {
+      case 37: // left
+        e.preventDefault();
+        move_left(ShapeArray[gen]);
+        superimpose.printSuper();
+        break;
+
+      case 38: // up
+        e.preventDefault();
+        superimpose.rotateShape(ShapeArray[gen],1);
+        superimpose.printSuper();
+        break;
+
+      case 39: // right
+        e.preventDefault();
+        move_right(ShapeArray[gen]);
+        superimpose.printSuper();
+        break;
+
+      case 40: // down
+        e.preventDefault();
+        condition = move_down(ShapeArray[gen]);
+        if (condition === 1){
+          gen = Math.floor((Math.random() * 4) + 1);
+          end_of_game();
+          //leave the older shape on grid  and insert a new one at the top!
+          superimpose.copyShape(ShapeArray[gen]);
+        }
+
+        superimpose.printSuper();
+        //printGrid();
+        break;
+
+      default:
+        break;
     }
-
-    switch(e.which || e.keyCode) {
- 		case 37: // left
-      e.preventDefault();
- 			move_left(ShapeArray[gen]);
- 			superimpose.printSuper();
-            // printGrid();
-        break;
-
-        case 38: // up
-          e.preventDefault();
-        	superimpose.rotateShape(ShapeArray[gen],1);
-        	superimpose.printSuper();
-            //printGrid();
-        break;
-
-        case 39: // right
-          e.preventDefault();
-        	move_right(ShapeArray[gen]);
-        	superimpose.printSuper();
-            //printGrid();
-        break;
-
-        case 40: // down
-          e.preventDefault();
-        	condition = move_down(ShapeArray[gen]);
-            if (condition === 1){
-                gen = Math.floor((Math.random() * 4) + 1);
-                end_of_game();
-                superimpose.copyShape(ShapeArray[gen]);            //leave the older shape on grid  and insert a new one at the top! 
-                
-                //printGrid();
-            }
-            
-       		superimpose.printSuper();
-            //printGrid();
-        break;
-
-        // case 33:  //page up
-        // 	superimpose.copyShape(ShapeArray[gen]);			//leave the older shape on grid  and insert a new one at the top! 
-        // 	superimpose.printSuper();
-        // break;
-
-        default:
-            // alert("Hello");
-            break;
-
-    }
+  }
 }
 
 function start(){
